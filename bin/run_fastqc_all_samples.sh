4190f9ba56155e70b456ddbbec2385e48f705546
Chris Probert <csprobert@gmail.com>
2016-02-12 08:42:38 -0800

    update fastqc wrapper

--- bin/run_fastqc_all_samples.sh
+++ bin/run_fastqc_all_samples.sh
@@ -4,6 +4,9 @@
 # Run fastqc on all RNA-seq samples
 #
 
+source /users/cprobert/.bashrc
+source /users/cprobert/dev/gecco-rna/bin/project_paths.sh
+
 # Load fastqc module
 module load fastqc/0.11.2
 
@@ -21,14 +24,20 @@ mkdir -p ${fastqc_output_dir}
 # Iterate through each batch, and run fastqc on all fastqs in the batch
 for batchdir in ${PROJECT_READS_BATCH_DIR_NAMES}; do
 
-    export seq_dir="${PROJECT_READS_BASE_DIR}/${batchdir}"
+    echo "Running ${batchdir}"
+
+    export seq_dir="${PROJECT_READS_BASE_DIR}/${batchdir}/"
     export output_fname_base="${batchdir}_"
     export output_path_base="${fastqc_output_dir}${output_fname_base}"
 
-    export fastqs=$(ls ${seq_dir} | sed "s/.fastq.gz//")
+    export fastqs=$(ls ${seq_dir}*R[12].fastq.gz | sed "s/.fastq.gz//")
+
+    for fq in ${fastqs}; do
+        mkdir -p ${output_path_base}$(basename ${fq})
+    done
 
     parallel -P 25% \
-        'zcat {}.fastq.gz | fastqc stdin --outdir=${output_path_base}{}'\
+        'zcat {}.fastq.gz | fastqc stdin --outdir=${output_path_base}{/}' \
         ::: ${fastqs}
 
 done
